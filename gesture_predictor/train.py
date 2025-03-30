d3c8cfb15d4671f5898dda72d53e18bfbc0bd8bf
ejdam87 <adam.dzadon@gmail.com>
2025-03-30 13:43:11 +0200

    feat: restructure predictor

--- gesture_predictor/train.py
+++ gesture_predictor/train.py
@@ -6,48 +6,7 @@ from sklearn.model_selection import train_test_split
 from dataset import HandDataset
 from models.stupid_net import StupidNet, init_weights
 from models.shallow_stupid_net import ShallowStupidNet
-
-def train_loop(
-        dataloader: DataLoader,
-        model: nn.Module, 
-        loss_fn: nn.Module,
-        optimizer: torch.optim.Optimizer,
-        batch_size: int) -> None:
-
-    size = len(dataloader.dataset)
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        pred = model(X)
-        loss = loss_fn(pred, y.long())
-
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * batch_size + len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-
-def test_loop(
-        dataloader: DataLoader,
-        model: nn.Module,
-        loss_fn: nn.Module) -> None:
-
-    model.eval()
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    test_loss, correct = 0, 0
-
-    with torch.no_grad():
-        for X, y in dataloader:
-            pred = model(X)
-            test_loss += loss_fn(pred, y.long()).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+from utils.training import train_loop, test_loop
 
 
 def main(hagrid_path: str, fabia_path: str) -> None:
